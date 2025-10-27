cee28a37f85a96240d5afac3e2f17f81e7ce55b4
Vitor Mateus <vmateusaguiar@gmail.com>
2025-10-27 16:40:41 -0300

    feat: implements User Details Service

--- backend/src/main/java/com/rokaly/sge/repository/UserRepository.java
+++ backend/src/main/java/com/rokaly/sge/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.rokaly.sge.repository;
 
 import com.rokaly.sge.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    UserDetails findByLogin(String login);
 }
