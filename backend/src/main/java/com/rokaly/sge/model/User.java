1d010300b1ef484b7d16b9574b4b6616e96f39bb
Vitor Mateus <vmateusaguiar@gmail.com>
2025-10-27 16:22:30 -0300

    feat: user entity created

---
+++ backend/src/main/java/com/rokaly/sge/model/User.java
@@ -0,0 +1,40 @@
+package com.rokaly.sge.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String login;
+    private String password;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
