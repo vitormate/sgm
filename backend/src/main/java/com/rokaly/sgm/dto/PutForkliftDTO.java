0c876ea2a0f70bce780bbd00866d801b160c359a
Vitor Mateus <vmateusaguiar@gmail.com>
2025-11-18 22:14:05 -0300

    feat: remove old files

--- backend/src/main/java/com/rokaly/sgm/dto/PutForkliftDTO.java
+++
@@ -1,6 +0,0 @@
-//package com.rokaly.sge.dto;
-//
-//import com.rokaly.sge.model.Type;
-//
-//public record PutForkliftDTO(Long id, Double hourMeter, Type type) {
-//}
--- backend/src/main/java/com/rokaly/sgm/repository/HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.rokaly.sge.repository;
-//// REMOVER
-//import com.rokaly.sge.model.History;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface HistoryRepository extends JpaRepository<History, Long> {
-//}
